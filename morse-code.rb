4d957707ae60a05c271b9fd8d2c51058d31a0579
redwan <redtah2007@yahoo.com>
2023-10-04 01:08:32 -0700

    Decode a word

--- morse-code.rb
+++ morse-code.rb
@@ -31,9 +31,25 @@
     @current_letter = @myWords[letter]
     @current_letter
   end
-  
 
+  def decode_word(word)
+    @letters = word.split
+    @current_word = ''
+    @letters.each do |letter|
+        decode_letter(letter)
+      @current_word += decode_letter(letter)
+    end
+    print "#{@current_word} "
+  end
+  
+  def decode_message(message)
+    @words = message.split('   ')
+    @words.each do |word|
+      decode_word(word)
+    end
+  end
+  
   decode_letter(".-")
-  # decode_word("-- -.--")
-  # decode_message("-- -.--   -. .- -- .")
-  # decode_message('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
+  decode_word("-- -.--")
+  decode_message("-- -.--   -. .- -- .")
+  decode_message('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
